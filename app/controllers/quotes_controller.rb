ca6efab25e13daf2ed42d0e718e1d3c05ed01849
Ashish Dixit <tundal45@gmail.com>
2020-07-12 09:47:48 -0700

    Display env data

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,6 @@
 class QuotesController < Rulers::Controller
   def a_quote
     # quote = system("fortune -s")
-    "There is nothing either good or bad but thinking makes it so."
+    "There is nothing either good or bad but thinking makes it so.\n<pre>\n#{env}\n</pre>"
   end
 end
