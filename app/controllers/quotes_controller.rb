b8afc310f62a75ff89205b12206047903a3c373f
Ashish Dixit <tundal45@gmail.com>
2020-09-01 16:14:30 -0700

    Finishes chapter 4 with views

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,5 @@
 class QuotesController < Rulers::Controller
   def a_quote
-    # quote = system("fortune -s")
-    "There is nothing either good or bad but thinking makes it so."
+    render :a_quote, :noun => :winking
   end
 end
